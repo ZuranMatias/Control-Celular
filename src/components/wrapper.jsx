3d6021078bef7f8fed0ad794b96b7dc74c6a52fa
MZuran <mzuran@frba.utn.edu.ar>
2024-01-22 12:18:37 -0300

    Fix: Wrapper

--- src/components/wrapper.jsx
+++ src/components/wrapper.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Wrapper({ color, children }) {
+function Wrapper({ color, children, title }) {
+  if (!title) {title = "Title"}
   return (
     <div className={`wrapper ${color}`}>
-      <div className={`title ${color}`}>{color}</div>
+      <div className={`title ${color}`}>{title}</div>
       <div style={{ maxWidth: '80%' }}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,16 +7,15 @@ import Separator from '../components/navbar/separator'
 function Home() {
   return (
     <>
-      <section>
-        <Header />
-      </section>
+      <section> <Header /> </section>
       <Separator color={"rgb(255,255,255)"}/>
       <section>
-        <div className='blank-container'>
+        <div className='blank-container d-flex flex-column'>
           <h1>Control Celular</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus mollitia quod, deserunt, totam quis maxime possimus at amet, commodi doloribus dicta velit sequi? Harum corporis minus fuga repellat dolore molestias!</p>
         </div>
       </section>
-      <Wrapper color={'blue'}> </Wrapper>
+      <Wrapper color={"light-blue"} title={"el control parental"}> </Wrapper>
       <Wrapper color={'red'}> </Wrapper>
       <Wrapper color={'black'}> </Wrapper>
     </>
