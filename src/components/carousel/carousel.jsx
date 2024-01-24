2e906c73a922954b50683d0b86b0d494bfe46431
MZuran <mzuran@frba.utn.edu.ar>
2024-01-24 11:31:40 -0300

    Add: Slide 4

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -6,6 +6,7 @@ import CarouselContent from './carousel_content'
 import Slide1 from './slides/slide1'
 import Slide2 from './slides/slide2'
 import Slide3 from './slides/slide3'
+import Slide4 from './slides/slide4'
 
 function HeaderCarousel({ height, upperMargin }) {
   if (!height) { height = '100%' }
@@ -23,6 +24,7 @@ function HeaderCarousel({ height, upperMargin }) {
         stopOnHover={false}
       >
         <CarouselContent height={height} backgroundNumber={5}> <Slide1 /></CarouselContent>
+        <CarouselContent height={height} backgroundNumber={6}> <Slide4 /></CarouselContent>
         <CarouselContent height={height} backgroundNumber={4}> <Slide2 /></CarouselContent>
         <CarouselContent height={height} backgroundNumber={1}> <Slide3 /></CarouselContent>
       </Carousel>
---
+++ src/components/carousel/slides/slide4.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Slide4() {
+  return (
+    <div className="carousel-content-container evenly-spaced text-left d-flex align-items-center justify-content-center">
+      <div>
+        <h2 className="carousel-title" style={{textAlign: 'center'}}> <strong> APLICACIÓN COMPLETAMENTE INVISIBLE </strong></h2>
+        <h3 style={{ marginTop: '1rem', textAlign: 'center'}}>
+          SEA SU ÁNGEL GUARDIAN CUIDÁNDOLOS DESDE SU PROPIO CELULAR
+          <hr style={{ width: '100%', margin: '1vh'}} />
+        </h3>
+        <h3 style={{textAlign: 'center'}}>SIN QUE ELLOS NUNCA SE ENTEREN</h3>
+        <p className='carousel-text desktop-only' style={{textAlign: 'center'}}>
+        ¡Respete la privacidad de su familia con nuestro sutil e indetectable software! <br />
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Slide4
