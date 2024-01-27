fbc92168c4d0652ed8b0972d741038de787132bc
MZuran <mzuran@frba.utn.edu.ar>
2024-01-27 11:34:36 -0300

    Tweaked: Contact

---
+++ src/components/dividers/verticalLine.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import aosOptions from '../AOS/AOS_Options'
+
+const CSS = {
+  borderLeft: '1px solid rgba(50,50,200,0.8)',
+  height: '300px',
+  width: '0px',
+  marginLeft: '-3px',
+  //top: '0',
+  borderWidth: ' 0 1px',
+  boxShadow: "2px 3px 3px 1px rgb(0 0 0 / 20%)",
+  alignSelf: "center",
+  marginLeft: "5vw",
+  marginRight: "5vw",
+}
+
+function VerticalLine() {
+  return (
+    <>
+      <div style={{ ...CSS}} {...aosOptions.fadeDown}></div>
+    </>
+  )
+}
+
+export default VerticalLine
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,6 +4,7 @@ import { WhatsappAPIButton } from '../components/buttons/contactButtons'
 import { GmailApiButton } from '../components/buttons/contactButtons'
 
 import Separator from '../components/separator'
+import VerticalLine from '../components/dividers/verticalLine'
 
 import { Form, Button } from 'react-bootstrap'
 
@@ -22,6 +23,7 @@ function submitted() {
 function Contacto() {
   return (
     <div className='d-flex flex-column'>
+      {/* Header */}
       <header className="d-flex flex-column align-items-center justify-content-center header-contact" style={{ width: '100%', height: '70vh' }} >
         <img className="header-contact-image" />
         <h1 {...aosOptions.fadeUp} >Contacto</h1>
@@ -29,18 +31,20 @@ function Contacto() {
       <Separator id="headerSeparator" scrollTargetId={'openingSection'} />
       <div id="openingSection" />
 
+      {/* Titles */}
       <div style={{marginTop: "5vw", textAlign: "center",paddingLeft: "5%",paddingRight: "5%"}}>
       <h2 className='grey-on-hover' {...aosOptions.fadeUp}>¡Conéctate con Nosotros!</h2>
       <h4 className='grey-on-hover' {...aosOptions.fadeUp}>No dudes en ponerte en contacto con nuestro equipo para cualquier pregunta, <br/> comentario o asistencia que requieras.</h4>
       </div>
 
-      <div style={{ alignSelf: "center" }} className="row-column justify-content-around contact-body responsive-padding" >
+      {/* Content */}
+      <div style={{ alignSelf: "center" }} className="row-column justify-content-center contact-body responsive-padding" >
 
-        <div className="d-flex flex-column align-items-center justify-content-around" >
+        <div className="d-flex flex-column align-items-center justify-content-center" >
           <div {...aosOptions.fadeUp}><WhatsappAPIButton /></div>
           <div {...aosOptions.fadeUp}><GmailApiButton /></div>
         </div>
-
+        <VerticalLine/>
         <section>
           <Form action="https://formsubmit.co/zuranmatias@gmail.com" method="POST" target="hiddenFrame" onSubmit={submitted}>
 
