775e870bc59dfa9a61509e09f394a6a4e1ace8cd
MZuran <mzuran@frba.utn.edu.ar>
2024-02-16 14:41:21 -0300

    Changed: FAQ questions

--- src/pages/faq/questionsList.jsx
+++ src/pages/faq/questionsList.jsx
@@ -1,71 +1,80 @@
 export const questionsList = [
   {
-    title: '¿Voy a tener algún gasto adicional si uso este software?',
+    title: '¿VOY A TENER MAYORES GASTOS POR EL USO DEL SOFTWARE?',
     content:
       'No, el software utiliza la conexión disponible en el teléfono, ya sea 3G, 4G o WiFi, y su consumo es prácticamente imperceptible.',
   },
   {
-    title: '¿Puede un antivirus detectar este software?',
+    title: '¿QUÉ ES CELULAR ESPIA ELITE?',
     content:
-      'No, este software está diseñado para evadir la detección de antivirus y no puede ser identificado por ellos.',
-  },
+      'Es un programa que se instala directamente en el Celular a controlar. El mismo trabajará en forma invisible para el usuario, recolectará todas las actividades y las enviará a un panel web privado al cual accede únicamente usted donde podrá revisar todas las grabaciones de voz, texto, videos e imágenes que se hicieron.<br/><br/> Hay dos maneras para instalar: <br/>La primera, puede instalar el sistema a través de nuestro manual con imágenes paso a paso y también instrucciones escritas. También podemos instalarlo en forma remota a través de una aplicación para que nosotros ingresemos al dispositivo y hagamos toda la instalación y configuración para luego eliminar dicha aplicación o todo rastro de la misma. Para instalar el programa en forma REMOTA tiene que tener en su mano el dispositivo como mínimo 15 minutos. <br/>La segunda manera es comprar un dispositivo nuevo, lo cual puede hacerlo usted mismo y luego se hace la instalación de acuerdo a la comodidad del cliente. Cualquier otra posibilidad no dude preguntarnos.',  },
   {
-    title:
-      'Si una persona borra las llamadas o mensajes del teléfono, ¿la información se pierde?',
-    content:
-      'No, la información nunca se pierde, ya que el sistema la registra instantáneamente cuando se genera. En el caso de que el teléfono no tenga conexión a Internet en ese momento, el programa la almacenará y la enviará al portal cuando se restablezca la conexión.',
-  },
-  {
-    title:
-      '¿Por cuánto tiempo puedo utilizar este programa para monitorear un celular?',
-    content:
-      'Ofrecemos varios períodos de uso: una versión de prueba gratuita de 2 días, una licencia de 15 días y licencias de 1, 3, 6 o 12 meses, para adaptarse a tus necesidades.',
-  },
-  {
-    title:
-      '¿Puedo instalar la aplicación en Android sin necesidad de hacer root?',
+    title: '¿PUEDO HACER LA INSTALACIÓN EN ANDROID SIN HACER ROOT?',
     content:
       'Sí, nuestra aplicación puede instalarse en dispositivos Android sin necesidad de realizar root. Esto significa que puedes aprovechar todas las funciones de monitoreo y seguridad que ofrece nuestra aplicación sin comprometer la integridad del sistema operativo de tu dispositivo. La instalación sin root es rápida, sencilla y segura, evitando posibles riesgos de seguridad y pérdida de garantía.',
   },
-
   {
-    title: '¿Puedo instalar la aplicación en iPhone/iOS?',
+    title: '¿POR QUÉ OPTAR POR ELITE?',
     content:
-      'La aplicación actualmente solo está disponible para dispositivos Android.',
-  },
-
-  {
-    title: '¿Por qué optar por elite?',
-    content:
-      'Para descubrir la verdad, anticiparse a eventos perjudiciales y proteger a los jóvenes del grooming, secuestros y acoso, con la capacidad de detectar riesgos y localizar en tiempo real.',
+      'Por este medio podemos proteger a nuestros hijos del Grooming, acoso, pedofilia, etc. También es muy útil para el caso de empleados infieles y en el caso familiar para sacarnos esa duda que nos carcome diariamente.',
   },
   {
-    title: '¿Puedo instalar Elite en un dispositivo al que no tengo acceso?',
+    title: '¿PUEDO INSTALAR EL SOFTWARE SOLO CON EL NÚMERO DE TELÉFONO?',
     content:
-      'En Android, la instalación se realiza físicamente. No es posible hacerlo sin tener el dispositivo en la mano.',
+      'Es imposible, no existe esa posibilidad. La instalación se realiza de forma física. No es posible hacerlo sin tener el dispositivo en su mano. <br/><strong>TODO LO QUE LE OFRECEN POR INTERNET CON SOLO EL NÚMERO ES UNA ESTAFA.</strong>',
   },
   {
-    title: '¿Cuántos dispositivos puedo monitorear con mi licencia elite?',
+    title: '¿CÓMO ES LA INSTALACIÓN REMOTA?',
     content:
-      'Solo puedes monitorear un dispositivo a la vez con una licencia elite. No obstante, puedes desactivar, desinstalar y transferir la aplicación a otros dispositivos tantas veces como necesites durante el período de tu licencia.',
+      'La instalación remota se hace cuando el cliente tiene en su mano el Celular a intervenir, debe descargar desde Google Play el programa (Teamviewer) que es gratuito y luego darnos acceso para realizar la instalación. La demora es de 10 minutos aproximadamente y es un servicio gratuito incluido con la compra de la licencia de 6 meses o un año. Si desea reinstalar o por cambio de dispositivo deberá pagar por la asistencia remota unos 50 u$D por cada instalación pero normalmente no es necesario ya que su instalación es muy fácil y tiene los manuales paso a paso como así también videos explicativos.',
   },
   {
-    title: '¿Es elite visible?',
+    title: '¿CUÁNTOS DISPOSITIVOS PUEDO MONITOREAR CON MI LICENCIA?',
     content:
-      'Elite es completamente invisible una vez instalado, y es indetectable incluso con una revisión detallada.',
+      'La licencia de Elite se puede utilizar solo en un dispositivo. Pero puede desactivar, desinstalar y mover la aplicación desde un dispositivo a otro de forma ILIMITADA mientras dure el periodo de su vigencia de su licencia. Ante pérdida o robo y también deseo de cambiar el móvil puede reinstalar nuevamente el software durante la vigencia de la licencia.',
   },
   {
-    title: '¿Cuánto tiempo se conservan los datos?',
+    title: '¿ES VISIBLE EL SOFTWARE ELITE?',
+    content:
+      'Elite es completamente invisible una vez instalado, y está programado para esconderse automáticamente.',
+  },
+  {
+    title: '¿CUÁNTO TIEMPO SE CONSERVAN LOS DATOS?',
     content:
       'Los datos almacenados en nuestros servidores se eliminan automáticamente y de manera permanente después de 90 días. Sin embargo, tienes la opción de descargar tus datos y guardarlos tú mismo.',
   },
   {
-    title: '¿Existe una versión de prueba gratuita de la aplicación?',
-    content: 'Sí, ofrecemos un período de prueba de 2 días sin cargo.',
+    title: '¿EXISTE UNA PRUEBA GRATUITA DE LA APLICACIÓN?',
+    content:
+      'Sí, ofrecemos un período de prueba de 2 días sin cargo y somos lo únicos en dar este período de prueba completamente gratis para que pueda ver el funcionamiento. Para obtener la prueba gratuita debe contactarnos al whatsapp. Además estamos en CABA, Argentina y puede venir personalmente a realizar la instalación previa comunicación por whatsapp.',
   },
   {
-    title: '¿La aplicación funciona sin conexión a internet?',
+    title: '¿PUEDO INSTALAR LA APLICACIÓN EN IPHONE/IOS?',
     content:
-      'No, en algún momento el dispositivo móvil deberá conectarse a internet mediante datos móviles o WiFi para poder enviar los datos recolectados. Se puede elegir si desea enviar los datos a través de la red móvil o WiFi.',
+      'La aplicación actualmente solo está disponible para dispositivos Android.',
+  },
+  {
+    title: '¿LA APLICACIÓN FUNCIONA SIN CONEXIÓN A INTERNET?',
+    content:
+      'No, en algún momento el dispositivo móvil deberá conectarse a internet mediante datos móviles o WiFi para poder enviar los datos recopilados. Se puede elegir si desea enviar los datos a través de la red móvil o WiFi.',
+  },
+  {
+    title: '¿DESDE QUÉ DISPOSITIVO PUEDO VER LA INFORMACIÓN?',
+    content:
+      'Puede ver la información recopilada en el sitio web ingresando el usuario (que nosotros le damos) y su clave personal, la cual puede cambiar las veces que lo desee, con cualquier dispositivo con acceso a internet, los cuales pueden ser su móvil, PC, tablet, notebook. Si desea renovar la licencia no necesitamos su clave personal, solo el usuario.',
+  },
+  {
+    title:
+      'SI BORRAN LAS LLAMADAS O MENSAJES DEL TELÉFONO, ¿LA INFORMACIÓN SE PIERDE?',
+    content:
+      'No, la información nunca se pierde, ya que el sistema la registra instantáneamente cuando se genera y se lo envía a su panel de control, en el teléfono pueden borrar lo que quieran pero cuando llega a su panel, ahí queda almacenado por 90 días.',
+  },
+  {
+    title: '¿PUEDE ALGUIEN VER MI PANEL DE CONTROL?',
+    content: 'No, es imposible, salvo usted con su usuario y clave.',
+  },
+  {
+    title: '¿ES POSIBLE SACAR FOTOS O FILMAR REMOTAMENTE?',
+    content: 'Sí, es posible solicitándolo a través del panel de control.',
   },
 ]
